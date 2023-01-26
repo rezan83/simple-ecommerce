425d18a3795de1734d634046a8b17c6eedfe94ea
rezan83 <rezan.moh83@gmail.com>
2023-01-26 12:11:42 +0100

    split code

--- main.js
+++ main.js
@@ -1,38 +1,10 @@
-// router code ########################################
-const allPages = document.getElementById("allPages");
-const { products, cart, login, detail } = allPages.children;
-
-const pages = {
-    products,
-    cart,
-    login,
-    detail
-};
-
-const selectPage = (event, showPage) => {
-    event.preventDefault();
-    for (const page in pages) {
-        pages[page].style.display = "none";
-    }
-    pages[showPage].style.display = "block";
-};
-
-// ########################################################
-const productContainer = document.querySelector(".product-container");
+// add remove and cart total calc ########################
 const cartIcon = document.querySelector(".cart-shopping-count");
 const cartProductContainer = document.querySelector(".cart__product-container");
 const cartTotal = document.getElementById("cart__total");
 const cartPayBtn = document.querySelector(".cart__pay-btn");
 
 let shoppingCart = [];
-const productsData = [
-    { id: 1, title: "cool car1", price: 12000 },
-    { id: 2, title: "cool car2", price: 11000 },
-    { id: 3, title: "cool car3", price: 10000 },
-    { id: 4, title: "cool car4", price: 9000 },
-    { id: 5, title: "cool car5", price: 8000 },
-    { id: 6, title: "cool car6", price: 7000 }
-];
 
 const updateCart = () => {
     cartProductContainer.innerHTML = cartPopulate();
@@ -53,9 +25,8 @@ const addProductToCart = (productId) => {
     );
     if (!is_productInCart) {
         shoppingCart.unshift({ ...product, count: 1 });
+        updateCart();
     }
-
-    updateCart();
 };
 
 const removeProductFromCart = (productId) => {
@@ -64,31 +35,13 @@ const removeProductFromCart = (productId) => {
 };
 
 const changeProductInCart = (event, productId) => {
-    let is_productInCart = shoppingCart.find(
+    let productInCart = shoppingCart.find(
         (product) => product.id === productId
     );
-    is_productInCart.count = event.target.value;
+    productInCart.count = event.target.value;
     cartTotal.innerHTML = `Total: $${cartTotalCalc(shoppingCart)}`;
     cartPayBtn.disabled = !shoppingCart.length;
 };
-const productsPopulate = productsData
-    .map((product) => {
-        return `<div class="product-card">
-                <img src="img/car2.webp" alt="product image" />
-                <div class="product-card-info">
-                    <h3>${product.title}</h3>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur
-                        adipisicing elit. Ducimus, ullam?
-                    </p>
-                    <p>$${product.price}</p>
-                    <button onclick="addProductToCart(${product.id})" class="btn btn-aqua product__add-btn">
-                        Add to cart
-                    </button>
-                </div>
-            </div>`;
-    })
-    .join("");
 
 const cartPopulate = () => {
     return shoppingCart
@@ -130,5 +83,4 @@ const cartPopulate = () => {
         .join("");
 };
 
-productContainer.innerHTML = productsPopulate;
-updateCart()
+updateCart();
